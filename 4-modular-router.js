be075b6750176e39b0c2774bb040211d57581b93
Iamthevk <vijay.vuex@gmail.com>
2024-02-01 22:15:01 +0530

    modular route handler

---
+++ 4-modular-router.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const miniapp = express.Router();
+
+miniapp.get("/home", (request, response, next) => {
+  const url = request.originalUrl;
+  response.status(200).send(`You are visiting /home from ${url}`);
+});
+app.use("/first", miniapp);
+app.use("/second", miniapp);
+
+app.listen(1339, () => console.log("Web Server running on port 1339"));
